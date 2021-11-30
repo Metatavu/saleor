70c98f77e4f2295489c85befe916788f2ce84e8c
Tianxing Wu <48071821+Chi-Chu319@users.noreply.github.com>
2021-11-30 22:07:18 +0200

    Update middleware.py
    cookie samesite

--- saleor/core/middleware.py
+++ saleor/core/middleware.py
@@ -134,6 +134,7 @@ def jwt_refresh_token_middleware(get_response):
                 JWT_REFRESH_TOKEN_COOKIE_NAME,
                 jwt_refresh_token,
                 expires=expires,
+                samesite="None",
                 httponly=True,  # protects token from leaking
                 secure=not settings.DEBUG,
             )
